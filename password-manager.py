5e1e546b02f1c7069adb04e92dc6aef640a44dde
szilvagyivilmos <vilmos.szilvagyi@gmail.com>
2018-04-21 14:10:10 +0200

    Update password-manager.py

--- password-manager.py
+++ password-manager.py
@@ -4,11 +4,11 @@ def validate_password(m_pass):
 	real_m_pass_file = open('passwords/master_password', 'r')
 	real_m_pass = real_m_pass_file.readline()
 	real_m_pass_file.close()
-	if m_pass != real_m_pass:
-		print 'Incorrect password'
+	if m_pass == real_m_pass:
+		print('Correct password!')
 		exit(0)
 
-	print 'Correct password!'
+	print('Incorrect password')
 
-m_pass = raw_input('Master password: ')
+m_pass = input('Master password: ')
 validate_password(m_pass)
