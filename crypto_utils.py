2acf2d8cc0b2b28342965142b0cf270921b7eaec
ommhoa <omar.osman.asd@gmail.com>
2018-04-30 15:56:35 +0200

    refactor & handle add in a dummy way

---
+++ crypto_utils.py
@@ -0,0 +1,118 @@
+import binascii
+
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import PBKDF2
+
+
+def _write_pass(file, m_pass, get_password):
+    salt = 'randomsalt'
+    iter = 1000
+    key = _generate_key(m_pass, salt, iter)
+
+    password = get_password()
+    tag, nonce, e_pass = _encrypt(key, password.encode())
+    _write_file(file, salt, tag, nonce, e_pass)
+
+    print('Password Saved!')
+
+
+def _encrypt(key, password):
+    cipher = AES.new(key, AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(password)
+
+    return tag, nonce, ciphertext
+
+
+def _generate_key(password, salt, iter):
+    key2 = binascii.hexlify(PBKDF2(password, salt))
+    return key2
+
+
+def _write_file(file, salt, tag, nonce, e_pass):
+    # print('Write\nsalt: '+salt+'\ntag: '+str(tag)+'\nnon: '+str(nonce)+'\nepass: '+str(e_pass))
+    with open('passwords/' + file, 'wb') as f:
+        f.write(salt.encode())
+        f.write('\n'.encode())
+        print((tag).decode('cp437'))
+        f.write((tag))
+        f.write('\n'.encode())
+        f.write((nonce))
+        f.write('\n'.encode())
+        f.write((e_pass))
+
+    return e_pass, salt
+
+
+class AccountHandler:
+    def __init__(self, domain, username):
+        self.domain = domain
+        self.username = username
+        self.file = domain + '_' + username
+
+
+class PasswordGenerator(AccountHandler):
+    def __init__(self, domain, username, difficulty, length):
+        super().__init__(domain, username)
+        self.difficulty = difficulty
+        self.length = length
+
+    def write_pass(self, m_pass):
+        _write_pass(self.file, m_pass, self.__generate_pass)
+
+    @staticmethod
+    def __generate_pass():
+        # TODO generate password with int self.difficulty, int self.length
+        # For help: ./password-manager.py generate --help
+        return "password"
+
+
+class PasswordWriter(AccountHandler):
+    def __init__(self, domain, username, password):
+        super().__init__(domain, username)
+        self.password = password
+
+    def write_pass(self, m_pass):
+        _write_pass(self.file, m_pass, self.get_pass)
+
+    def get_pass(self):
+        return self.password
+
+
+class PasswordReader(AccountHandler):
+    def read_pass(self, m_pass):
+        e_pass, salt, tag, nonce = self.__read_file(self.file)
+        key = _generate_key(m_pass, salt, tag)
+
+        password = self.__decrypt(key, e_pass, nonce, tag)
+
+        print(password)
+
+    @staticmethod
+    def __read_file(file):
+        with open('passwords/' + file, 'rb') as f:
+            salt = f.readline().rstrip()
+            tag = f.readline().rstrip()
+            nonce = f.readline().rstrip()
+            e_pass = f.readline()
+        # print('Read\nsalt: ' + salt + '\ntag: ' + str(tag) + '\nnon: ' + str(nonce) + '\nepass: ' + str(e_pass))
+        return e_pass, salt, tag, nonce
+
+    @staticmethod
+    def __decrypt(k, ciphertext, nonce, tag):
+        '''
+        nonce=nonce.encode('ISO-8859-1')
+        tag = bytes(tag, 'ISO-8859-1')
+        ciphertext=bytes(ciphertext, 'ISO-8859-1')
+        '''
+
+        cipher = AES.new(k, AES.MODE_EAX, nonce=nonce)
+        plaintext = cipher.decrypt(ciphertext)
+        try:
+            cipher.verify(tag)
+            print("The message is authentic:", plaintext)
+
+            return plaintext
+
+        except ValueError:
+            print("Key incorrect or message corrupted")
--- password-manager.py
+++ password-manager.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
-import binascii
 import sys
 from getpass import getpass
-
-from Crypto.Cipher import AES
-from Crypto.Protocol.KDF import PBKDF2
+from crypto_utils import PasswordWriter, PasswordGenerator, PasswordReader
 
 
 def main():
@@ -22,13 +19,6 @@ def main():
 def parse_args():
     parser = get_parser()
     args = parser.parse_args()
-    return vars(args)
-
-
-def parse_command(cmd):
-    parser = get_parser()
-    split_command = cmd.split(' ')
-    args = parser.parse_args(split_command)
     return vars(args)
 
 
@@ -61,6 +51,13 @@ def handle_args(args):
     print('There are some args')
 
 
+def parse_command(cmd):
+    parser = get_parser()
+    split_command = cmd.split(' ')
+    args = parser.parse_args(split_command)
+    return vars(args)
+
+
 def exit(command, m_pass):
     print('Goodbye!')
     sys.exit(0)
@@ -68,16 +65,20 @@ def exit(command, m_pass):
 
 def add(command, m_pass):
     print('add called')
+    writer = PasswordWriter('facebook', 'user', 'dummyPass')
+    writer.write_pass(m_pass)
 
 
 def generate(command, m_pass):
     print('generate called')
-    create_pass('facebook_user', m_pass)
+    writer = PasswordGenerator('facebook', 'user', 3, 8)
+    writer.write_pass(m_pass)
 
 
 def get(command, m_pass):
     print('get called')
-    read_pass('facebook_user', m_pass)
+    reader = PasswordReader('facebook', 'user')
+    reader.read_pass(m_pass)
 
 
 def print_wrong_command(wrong_command, m_pass):
@@ -97,91 +98,6 @@ def shell_mode(master_password):
     while True:
         command = input()
         actions.get(command, print_wrong_command)(command, master_password)
-
-
-def generate_key(password, salt, iter):
-    key2 = binascii.hexlify(PBKDF2(password, salt))
-
-    return key2
-
-
-def read_file(file):
-    with open('passwords/' + file, 'rb') as f:
-        salt = f.readline().rstrip()
-        tag = f.readline().rstrip()
-        nonce = f.readline().rstrip()
-        e_pass = f.readline()
-    # print('Read\nsalt: ' + salt + '\ntag: ' + str(tag) + '\nnon: ' + str(nonce) + '\nepass: ' + str(e_pass))
-    return e_pass, salt, tag, (nonce)
-
-
-def write_file(file, salt, tag, nonce, e_pass):
-    # print('Write\nsalt: '+salt+'\ntag: '+str(tag)+'\nnon: '+str(nonce)+'\nepass: '+str(e_pass))
-    with open('passwords/' + file, 'wb') as f:
-        f.write(salt.encode())
-        f.write('\n'.encode())
-        print((tag).decode('cp437'))
-        f.write((tag))
-        f.write('\n'.encode())
-        f.write((nonce))
-        f.write('\n'.encode())
-        f.write((e_pass))
-
-    return e_pass, salt
-
-
-def generate_pass(a, A, num, spec, length):
-    # TODO generate password with parameters bool a-z,bool A-Z,bool number,bool spec,int length
-
-    return "password"
-
-
-def encrypt(key, password):
-    cipher = AES.new(key, AES.MODE_EAX)
-    nonce = cipher.nonce
-    ciphertext, tag = cipher.encrypt_and_digest(password)
-
-    return (tag, nonce, ciphertext)
-
-
-def decrypt(k, ciphertext, nonce, tag):
-    '''
-    nonce=nonce.encode('ISO-8859-1')
-    tag = bytes(tag, 'ISO-8859-1')
-    ciphertext=bytes(ciphertext, 'ISO-8859-1')
-    '''
-
-    cipher = AES.new(k, AES.MODE_EAX, nonce=nonce)
-    plaintext = cipher.decrypt(ciphertext)
-    try:
-        cipher.verify(tag)
-        print("The message is authentic:", plaintext)
-
-        return plaintext
-
-    except ValueError:
-        print("Key incorrect or message corrupted")
-
-
-def create_pass(file, m_pass):
-    salt = 'randomsalt'
-    iter = 1000
-    key = generate_key(m_pass, salt, iter)
-
-    password = generate_pass(True, False, False, False, 8)
-    tag, nonce, e_pass = encrypt(key, password.encode())
-    write_file(file, salt, tag, nonce, e_pass)
-
-    print('Password Saved!')
-
-
-def read_pass(file, m_pass):
-    e_pass, salt, tag, nonce = read_file(file)
-    key = generate_key(m_pass, salt, tag)
-
-    password = decrypt(key, e_pass, nonce, tag)
-
-    print(password)
 
 
 main()
