0fceead25139cea63ba73bf92210fa25f3f8147e
ommhoa <omar.osman.asd@gmail.com>
2018-05-01 11:58:56 +0200

    refactor crypto utils

--- crypto_utils.py
+++ crypto_utils.py
@@ -3,26 +3,7 @@ import binascii
 from Crypto.Cipher import AES
 from Crypto.Protocol.KDF import PBKDF2
 from getpass import getpass
-
-
-def _write_pass(file, m_pass, get_password):
-    salt = 'randomsalt'
-    iter = 1000
-    key = _generate_key(m_pass, salt, iter)
-
-    password = get_password()
-    tag, nonce, e_pass = _encrypt(key, password.encode())
-    _write_file(file, salt, tag, nonce, e_pass)
-
-    print('Password Saved!')
-
-
-def _encrypt(key, password):
-    cipher = AES.new(key, AES.MODE_EAX)
-    nonce = cipher.nonce
-    ciphertext, tag = cipher.encrypt_and_digest(password)
-
-    return tag, nonce, ciphertext
+from abc import ABC, abstractmethod
 
 
 def _generate_key(password, salt, iter):
@@ -30,53 +11,85 @@ def _generate_key(password, salt, iter):
     return key2
 
 
-def _write_file(file, salt, tag, nonce, e_pass):
-    # print('Write\nsalt: '+salt+'\ntag: '+str(tag)+'\nnon: '+str(nonce)+'\nepass: '+str(e_pass))
-    with open('passwords/' + file, 'wb') as f:
-        f.write(salt.encode())
-        f.write('\n'.encode())
-        # print((tag).decode('cp437'))
-        f.write(tag)
-        f.write('\n'.encode())
-        f.write(nonce)
-        f.write('\n'.encode())
-        f.write(e_pass)
-
-    return e_pass, salt
-
-
 class AccountHandler:
+    domain: str
+    username: str
+    file: str
+
     def __init__(self, domain, username):
         self.domain = domain
         self.username = username
         self.file = domain + '_' + username
 
+    def write_db(self):
+        with open('passwords/db', 'ab') as f:
+            f.write(self.file.encode())
 
-class PasswordGenerator(AccountHandler):
+
+class DataWriter(ABC, AccountHandler):
+
+    def __init__(self, domain, username):
+        super().__init__(domain, username)
+
+    def write_pass(self, m_pass):
+        salt = 'randomsalt'
+        iter = 1000
+        key = _generate_key(m_pass, salt, iter)
+
+        password = self._get_password()
+        tag, nonce, e_pass = self.__encrypt(key, password.encode())
+        self.__write_file(self.file, salt, tag, nonce, e_pass)
+
+        print('Password Saved!')
+
+    @staticmethod
+    def __encrypt(key, password):
+        cipher = AES.new(key, AES.MODE_EAX)
+        nonce = cipher.nonce
+        ciphertext, tag = cipher.encrypt_and_digest(password)
+
+        return tag, nonce, ciphertext
+
+    @staticmethod
+    def __write_file(file, salt, tag, nonce, e_pass):
+        # print('Write\nsalt: '+salt+'\ntag: '+str(tag)+'\nnon: '+str(nonce)+'\nepass: '+str(e_pass))
+        with open('passwords/' + file, 'wb') as f:
+            f.write(salt.encode())
+            f.write('\n'.encode())
+            # print((tag).decode('cp437'))
+            f.write(tag)
+            f.write('\n'.encode())
+            f.write(nonce)
+            f.write('\n'.encode())
+            f.write(e_pass)
+
+        return e_pass, salt
+
+    @abstractmethod
+    def _get_password(self):
+        raise NotImplementedError
+
+
+class PasswordGenerator(DataWriter):
+    difficulty: int
+    length: int
+
     def __init__(self, domain, username, difficulty, length):
         super().__init__(domain, username)
         self.difficulty = difficulty
         self.length = length
 
-    def write_pass(self, m_pass):
-        _write_pass(self.file, m_pass, self.__generate_pass)
-
-    @staticmethod
-    def __generate_pass():
+    def _get_password(self):
         # TODO generate password with int self.difficulty, int self.length
         # For help: ./password-manager.py generate --help
         return "password"
 
 
-class PasswordWriter(AccountHandler):
+class PasswordWriter(DataWriter):
     def __init__(self, domain, username):
         super().__init__(domain, username)
 
-    def write_pass(self, m_pass):
-        _write_pass(self.file, m_pass, self.__prompt_pass)
-
-    @staticmethod
-    def __prompt_pass():
+    def _get_password(self):
         return getpass('Password: ')
 
 
